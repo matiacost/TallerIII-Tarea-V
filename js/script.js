ce9407876f60f187ae5fdc3aecd3d8510db7a40d
Matias Acosta <matiacost245@gmail.com>
2016-09-17 18:42:22 -0500

    Tarea V - Matias Acosta

---
+++ js/script.js
@@ -0,0 +1,24 @@
+alert('Abra la Consola con la tecla F12');
+
+(function(){ 
+	console.log('Hola Mundo');
+ })(); 
+ 
+ 
+ var descripcion = ''; 
+ var cantidad = 0; 
+ 
+ 
+ var array = []; 
+ array.push('Hola Mundo'); 
+ array.push('JavaScript'); 
+ console.log(array);  
+ 
+var objeto = {}  
+ 
+ objeto.cantidad=10; 
+ objeto.descripcion='Descripcion'; 
+ console.log(objeto); 
+ (function(){
+ 	console.log('Matias Acosta');
+ })();
